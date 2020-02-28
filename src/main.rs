b61538c9fc1d30a20b7e9dae5b56163c468a396c
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-02-28 10:46:44 +0100

    Use array of strings as data structure

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 extern crate sdl2;
 
+use std::time::Instant;
 use sdl2::pixels::Color;
 use sdl2::surface::Surface;
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
-#[derive(Default)]
 struct Editor {
-    text: String,
+    buffer_lines: Vec<String>,
 
     cursor_x: u32,
     cursor_y: u32,
+    cursor_animation_instant: Instant,
+}
+impl Default for Editor {
+    fn default() -> Self {
+        Self {
+            buffer_lines: vec![ String::from("") ],
+            cursor_x: 0,
+            cursor_y: 0,
+            cursor_animation_instant: Instant::now(),
+        }
+    }
 }
 impl Editor {
     pub fn move_cursor(&mut self, x: i32, y: i32) {
         if self.cursor_x == 0 && x < 0 {
             if self.cursor_y != 0 {
                 self.cursor_y -= 1;
-                let lines: Vec<&str> = self.text.split('\n').collect();
-                self.cursor_x = lines.get(self.cursor_y as usize).unwrap_or(&"").len() as u32;
+                self.cursor_x = self.buffer_lines.get(self.cursor_y as usize).unwrap_or(&String::from("")).len() as u32;
             }
         } else {
             self.cursor_x = ((self.cursor_x as i32) + x) as u32;
@@ -30,14 +40,13 @@ impl Editor {
         } else {
             self.cursor_y = ((self.cursor_y as i32) + y) as u32;
         }
-        self.cursor_frame = 0;
+        self.cursor_animation_instant = Instant::now();
     }
 }
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let sdl_video = sdl_context.video().unwrap();
-    let mut sdl_timer = sdl_context.timer().unwrap();
     let sdl_ttf = sdl2::ttf::init().unwrap();
 
     let cousine = sdl_ttf.load_font("./Cousine-Regular.ttf", 14).unwrap();
@@ -54,12 +63,12 @@ fn main() {
     let background_color = Color::RGB(250, 250, 250);
 
     let mut editor: Editor = Default::default();
-    editor.text = String::from("#include <stdio.h>
+    editor.buffer_lines = String::from("#include <stdio.h>
 
 int main(int argc, char **argv) {
     printf(\"%s\", \"Hello World!\");
     return 0;
-}");
+}").split('\n').map(String::from).collect();
 
     let window = sdl_video.window("ttttt...", character_width*characters_wide, character_height*characters_high)
         .position_centered()
@@ -83,19 +92,23 @@ int main(int argc, char **argv) {
                     break 'running;
                 },
                 Event::KeyDown { keycode: Some(Keycode::Backspace), .. } => {
+                    /*
                     if !editor.text.is_empty() {
                         editor.text = editor.text.chars().take(editor.text.len()-1).collect();
                         editor.move_cursor(-1, 0);
                     }
+                    */
                 },
                 Event::KeyDown { keycode: Some(Keycode::Return), .. } => {
-                    editor.text.push('\n');
+                    editor.buffer_lines.push(String::from(""));
                     editor.move_cursor(0, 1);
                     editor.cursor_x = 0;
                 },
                 Event::TextInput { text, .. } => {
-                    editor.text = format!("{}{}", editor.text, text);
-                    editor.move_cursor(1, 0);
+                    if let Some(last_line) = editor.buffer_lines.last_mut() {
+                        *last_line = format!("{}{}", last_line, text);
+                        editor.move_cursor(1, 0);
+                    }
                 },
                 Event::KeyDown { keycode: Some(Keycode::Left), .. } => {
                     editor.move_cursor(-1, 0);
@@ -116,7 +129,7 @@ int main(int argc, char **argv) {
         canvas.set_draw_color(background_color);
         canvas.clear();
 
-        for (line_index, line) in editor.text.split('\n').enumerate() {
+        for (line_index, line) in editor.buffer_lines.iter().enumerate() {
             if line.is_empty() { continue; }
             let text_surface = cousine.render(line).blended(foreground_color).unwrap();
             let texture = texture_creator.create_texture_from_surface(&text_surface).unwrap();
@@ -127,7 +140,7 @@ int main(int argc, char **argv) {
         }
 
         let cursor_color_ms_interval = 500;
-        let elapsed_ms = sdl_timer.ticks();
+        let elapsed_ms = editor.cursor_animation_instant.elapsed().as_millis();
         let cursor_color = if (elapsed_ms / cursor_color_ms_interval) % 2 == 0 {
             foreground_color
         } else {
