20f05c951b433015c6ee4f5e19ba421e94e4a1c7
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-03-04 17:42:33 +0100

    Fuck It I'm std::rc::Rc

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,25 +1,19 @@
+use std::rc::Rc;
 #[derive(Copy, Clone)]
 enum PieceSource {
     ORIGINAL,
     APPEND,
 }
-
-type PiecePointer = Option<usize>;
-
 struct Piece {
     start: usize,
     length: usize,
     source: PieceSource,
-
-    next: PiecePointer,
-    prev: PiecePointer,
+    next: Option<Rc<Piece>>,
 }
 pub struct Buffer {
     original: Vec<char>,
     append: Vec<char>,
-    pieces: Vec<Piece>,
-
-    first: PiecePointer,
+    first: Option<Rc<Piece>>,
 }
 
 use PieceSource::*;
@@ -28,7 +22,6 @@ impl Buffer {
         Self {
             original: Vec::new(),
             append: Vec::new(),
-            pieces: Vec::new(),
             first: None,
         }
     }
@@ -38,122 +31,70 @@ impl Buffer {
             length: source.len(),
             source: ORIGINAL,
             next: None,
-            prev: None,
         };
         Self {
             original: source.chars().collect(),
             append: Vec::new(),
-            pieces: vec![ first_piece ],
-            first: Some(0),
+            first: Some(Rc::new(first_piece)),
         }
     }
-    pub fn insert(&mut self, text: &str, pos: usize) {
+    pub fn insert(&mut self, text: &str, insert_position: usize) {
         let start = self.append.len();
         for c in text.chars() {
             self.append.push(c);
         }
 
-        let mut search_pos = 0;
+        let mut search_position = 0;
         let mut iter_piece = &self.first;
-        loop {
-            if let Some(piece_pointer) = *iter_piece {
-                let piece = &self.pieces[piece_pointer];
+        while let Some(piece) = iter_piece {
 
-                let piece_pos_start = search_pos;
-                search_pos += piece.length;
-                let piece_pos_end = search_pos;
+            let piece_position_start = search_position;
+            search_position += piece.length;
+            let piece_position_end = search_position;
 
-                if search_pos >= pos {
-                    if pos == piece_pos_start {
-                        let first_pointer = Some(self.pieces.len());
-                        let new_pointer = Some(self.pieces.len() + 1);
+            if search_position >= insert_position {
+                if insert_position == piece_position_start {
+                } else {
+                    let after = Piece{
+                        start: piece.start + piece.length,
+                        length: insert_position - piece_position_start,
+                        source: piece.source,
+                        next: piece.next.clone(),
+                    };
+                    let new = Piece{ 
+                        start: start,
+                        length: text.len(),
+                        source: APPEND,
+                        next: Some(Rc::new(after)),
+                    };
+                    let before = Piece {
+                        start: piece.start,
+                        length: piece_position_end - insert_position,
+                        source: piece.source,
+                        next: Some(Rc::new(new)),
+                    };
 
-                        let piece_prev = piece.prev;
-
-                        let first = Piece {
-                            start: piece.start,
-                            length: piece_pos_end - pos,
-                            source: piece.source,
-                            next: piece.next,
-                            prev: new_pointer,
-                        };
-                        self.pieces.push(first);
-
-                        let new = Piece{ 
-                            start: start,
-                            length: text.len(),
-                            source: APPEND,
-                            next: first_pointer,
-                            prev: piece_prev,
-                        };
-                        self.pieces.push(new);
-
-                        if piece_prev == None {
-                            self.first = new_pointer;
-                        }
-                    } else {
-                        let first_pointer = Some(self.pieces.len());
-                        let second_pointer = Some(self.pieces.len() + 1);
-                        let new_pointer = Some(self.pieces.len() + 2);
-
-                        let first = Piece {
-                            start: piece.start,
-                            length: piece_pos_end - pos,
-                            source: piece.source,
-                            next: new_pointer,
-                            prev: piece.prev,
-                        };
-                        let second = Piece{
-                            start: piece.start + piece.length,
-                            length: piece.length - first.length,
-                            source: piece.source,
-                            next: piece.next,
-                            prev: new_pointer,
-                        };
-                        self.pieces.push(first);
-                        self.pieces.push(second);
-
-                        let new = Piece{ 
-                            start: start,
-                            length: text.len(),
-                            source: APPEND,
-                            next: second_pointer,
-                            prev: first_pointer,
-                        };
-                        self.pieces.push(new);
-
-                        if let Some(p) = self.first {
-                            if p == piece_pointer {
-                                self.first = first_pointer;
-                            }
-                        }
+                    if let Some(piece) = &self.first {
+                        self.first = Some(Rc::new(before));
                     }
-                    break;
                 }
-                iter_piece = &piece.next;
-            } else {
                 break;
             }
+            iter_piece = &piece.next;
         }
     }
 
     pub fn to_string(&self) -> String {
         let mut result = String::new();
-        let mut iter_pointer = &self.first;
-        loop {
-            match iter_pointer {
-                None => break,
-                Some(pointer) => {
-                    let piece = &self.pieces[*pointer];
-                    let from = match &piece.source {
-                        ORIGINAL => &self.original,
-                        APPEND => &self.append,
-                    };
-                    result += from[piece.start..piece.start+piece.length].iter().collect::<String>().as_str();
+        let mut iter_piece = &self.first;
+        while let Some(piece) = iter_piece {
+            let from = match &piece.source {
+                ORIGINAL => &self.original,
+                APPEND => &self.append,
+            };
+            result += from[piece.start..piece.start+piece.length].iter().collect::<String>().as_str();
 
-                    iter_pointer = &piece.next;
-                },
-            }
+            iter_piece = &piece.next;
         }
         result
     }
