48bcc18ba861aaa49fd985cd8317eb627f8a3156
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-03-05 10:26:40 +0100

    Fuck It I'm contain-rs

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,19 +1,21 @@
-use std::rc::Rc;
-#[derive(Copy, Clone)]
+extern crate linked_list;
+use linked_list::LinkedList;
+
+#[derive(Copy, Clone, Debug)]
 enum PieceSource {
     ORIGINAL,
     APPEND,
 }
+#[derive(Debug)]
 struct Piece {
     start: usize,
     length: usize,
     source: PieceSource,
-    next: Option<Rc<Piece>>,
 }
 pub struct Buffer {
     original: Vec<char>,
     append: Vec<char>,
-    first: Option<Rc<Piece>>,
+    pieces: LinkedList<Piece>,
 }
 
 use PieceSource::*;
@@ -22,7 +24,7 @@ impl Buffer {
         Self {
             original: Vec::new(),
             append: Vec::new(),
-            first: None,
+            pieces: LinkedList::new(),
         }
     }
     pub fn from(source: &str) -> Self {
@@ -30,12 +32,13 @@ impl Buffer {
             start: 0, 
             length: source.len(),
             source: ORIGINAL,
-            next: None,
         };
+        let mut pieces = LinkedList::new();
+        pieces.push_back(first_piece);
         Self {
             original: source.chars().collect(),
             append: Vec::new(),
-            first: Some(Rc::new(first_piece)),
+            pieces,
         }
     }
     pub fn insert(&mut self, text: &str, insert_position: usize) {
@@ -45,56 +48,73 @@ impl Buffer {
         }
 
         let mut search_position = 0;
-        let mut iter_piece = &self.first;
-        while let Some(piece) = iter_piece {
+        let mut cursor = self.pieces.cursor();
+        while let Some(piece) = cursor.next() {
 
             let piece_position_start = search_position;
             search_position += piece.length;
             let piece_position_end = search_position;
 
             if search_position >= insert_position {
-                if insert_position == piece_position_start {
-                } else {
-                    let after = Piece{
-                        start: piece.start + piece.length,
-                        length: insert_position - piece_position_start,
-                        source: piece.source,
-                        next: piece.next.clone(),
-                    };
-                    let new = Piece{ 
-                        start: start,
-                        length: text.len(),
-                        source: APPEND,
-                        next: Some(Rc::new(after)),
-                    };
-                    let before = Piece {
-                        start: piece.start,
-                        length: piece_position_end - insert_position,
-                        source: piece.source,
-                        next: Some(Rc::new(new)),
-                    };
+                let piece_start = piece.start;
+                let piece_source = piece.source;
 
-                    if let Some(piece) = &self.first {
-                        self.first = Some(Rc::new(before));
+                cursor.prev();
+                cursor.remove();
+                
+                if insert_position != piece_position_end - 1 {
+                    let after = Piece{
+                        start: piece_start + insert_position - piece_position_start,
+                        length: piece_position_end - insert_position,
+                        source: piece_source,
+                    };
+                    if after.length > 0 {
+                        cursor.insert(after);
+                    }
+                }
+
+                let new = Piece{ 
+                    start: start,
+                    length: text.len(),
+                    source: APPEND,
+                };
+                if new.length > 0 {
+                    cursor.insert(new);
+                }
+
+                if insert_position != piece_position_start {
+                    let before = Piece {
+                        start: piece_start,
+                        length: insert_position - piece_position_start,
+                        source: piece_source,
+                    };
+                    if before.length > 0 {
+                        cursor.insert(before);
                     }
                 }
                 break;
             }
-            iter_piece = &piece.next;
         }
     }
 
-    pub fn to_string(&self) -> String {
+    pub fn debug_print_pieces(&mut self)  {
+        let cursor = &mut self.pieces.cursor();
+        println!("++++++++++++++++++++++++++++++++++++++++++++++");
+        while let Some(piece) = cursor.next() {
+            println!("{:?}", piece);
+        }
+        println!("----------------------------------------------");
+    }
+
+    pub fn to_string(&mut self) -> String {
         let mut result = String::new();
-        let mut iter_piece = &self.first;
-        while let Some(piece) = iter_piece {
+        let mut cursor = self.pieces.cursor();
+        while let Some(piece) = cursor.next() {
             let from = match &piece.source {
                 ORIGINAL => &self.original,
                 APPEND => &self.append,
             };
             result += from[piece.start..piece.start+piece.length].iter().collect::<String>().as_str();
-
-            iter_piece = &piece.next;
         }
         result
     }
