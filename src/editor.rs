b2c232562cc0b3b352333990fd0388731432f399
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-04-15 17:38:48 +0200

    Separate horizontal and vertical cursor logic

--- src/editor.rs
+++ src/editor.rs
@@ -46,37 +46,42 @@ impl Editor {
             matching_input_timeout: Duration::from_secs(1),
         }
     }
-    pub fn move_cursor(&mut self, render: &RenderContext, x: i32, y: i32) {
+
+    pub fn move_cursor_horizontal(&mut self, x: i64) {
+        let length_of_line = self
+            .buffer
+            .to_string()
+            .split('\n')
+            .nth(self.cursor_y)
+            .map(|it| UnicodeSegmentation::graphemes(it, true).count())
+            .unwrap_or(0);
+
+        let moving_beyond_first = x < 0 && self.global_cursor_x == 0;
+        let moving_beyond_last = x > 0 && self.global_cursor_x >= length_of_line;
+
+        if !moving_beyond_first && !moving_beyond_last {
+            self.global_cursor_x = ((self.global_cursor_x as i64) + x) as usize;
+        }
+
+        self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
+    }
+
+    pub fn move_cursor_vertical(&mut self, y: i64, render: &RenderContext) {
         let buffer_string = self.buffer.to_string();
         let buffer_lines: Vec<_> = buffer_string
             .split('\n')
             .map(|it| UnicodeSegmentation::graphemes(it, true).collect::<Vec<_>>())
             .collect();
 
-        if self.global_cursor_x == 0 && x < 0 {
-            if self.cursor_y != 0 {
-                self.cursor_y -= 1;
-                self.global_cursor_x = buffer_lines.get(self.cursor_y)
-                    .unwrap_or(&Vec::new()).len();
-            }
-        } else {
-            self.global_cursor_x = ((self.global_cursor_x as i32) + x) as usize;
+        let moving_beyond_first = y < 0 && self.cursor_y == 0;
+        let moving_beyond_last = y > 0 && self.cursor_y >= buffer_lines.len()-1;
+
+        if !moving_beyond_first && !moving_beyond_last {
+            self.cursor_y = ((self.cursor_y as i64) + y) as usize;
         }
 
-        if self.cursor_y == 0 && y < 0 {
-            self.cursor_y = 0;
-        } else {
-            self.cursor_y = ((self.cursor_y as i32) + y) as usize;
-        }
-
-        if self.cursor_y >= buffer_lines.len() {
-            self.cursor_y = buffer_lines.len() - 1;
-        }
-        if self.global_cursor_x >= buffer_lines[self.cursor_y].len() {
-            self.actual_cursor_x = buffer_lines[self.cursor_y].len();
-        } else {
-            self.actual_cursor_x = self.global_cursor_x;
-        }
+        let length_of_line = buffer_lines[self.cursor_y].len();
+        self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
 
         let ch = render.character_height;
         let window_height_in_characters = (render.height() / ch) as usize;
@@ -86,7 +91,7 @@ impl Editor {
             self.y_render_offset += y as usize;
         }
         if y < 0 && self.cursor_y < self.y_render_offset + 5
-            && (self.y_render_offset as i32) + y >= 0
+            && (self.y_render_offset as i64) + y >= 0
         {
             self.y_render_offset -= (-y) as usize;
         }
@@ -135,10 +140,10 @@ impl Editor {
         let mim: &[u32] = &self.matching_input_modifs;
         match (mit, mim) {
             binding!(i) => self.mode = Mode::INSERT,
-            binding!(h) => self.move_cursor(render, -1, 0),
-            binding!(l) => self.move_cursor(render, 1, 0),
-            binding!(k) => self.move_cursor(render, 0, -1),
-            binding!(j) => self.move_cursor(render, 0, 1),
+            binding!(h) => self.move_cursor_horizontal(-1),
+            binding!(l) => self.move_cursor_horizontal(1),
+            binding!(k) => self.move_cursor_vertical(-1, render),
+            binding!(j) => self.move_cursor_vertical(1, render),
             binding!(d, d) => println!("dd is nice!"),
             binding!(CTRL|a, b, CTRL|c) => println!("abc is nice!"),
             _ => reset_matching_input = false,
@@ -157,7 +162,7 @@ impl Editor {
         match (mit, mim) {
             binding!(BACKSPACE) => {
                 if self.actual_cursor_x != 0 || self.cursor_y != 0 {
-                    self.move_cursor(render, -1, 0);
+                    self.move_cursor_horizontal(-1);
                     let pos = self.cursor_position_in_buffer();
                     self.buffer.remove(pos);
                 }
@@ -165,14 +170,14 @@ impl Editor {
             binding!(RETURN) => {
                 let pos = self.cursor_position_in_buffer();
                 self.buffer.insert("\n", pos);
-                self.move_cursor(render, 0, 1);
+                self.move_cursor_vertical(1, render);
                 self.global_cursor_x = 0;
                 self.actual_cursor_x = 0;
             }
-            binding!(LEFT) => self.move_cursor(render, -1, 0),
-            binding!(RIGHT) => self.move_cursor(render, 1, 0),
-            binding!(UP) => self.move_cursor(render, 0, -1),
-            binding!(DOWN) => self.move_cursor(render, 0, 1),
+            binding!(LEFT) => self.move_cursor_horizontal(-1),
+            binding!(RIGHT) => self.move_cursor_horizontal(1),
+            binding!(UP) => self.move_cursor_vertical(-1, render),
+            binding!(DOWN) => self.move_cursor_vertical(1, render),
             binding!(CTRL|o) => {
                 let result = nfd::open_file_dialog(None, None)
                     .unwrap_or_else(panic_with_dialog);
@@ -196,7 +201,7 @@ impl Editor {
             _ if is_text_input => {
                 let pos = self.cursor_position_in_buffer();
                 self.buffer.insert(input, pos);
-                self.move_cursor(&render, 1, 0);
+                self.move_cursor_horizontal(1);
             },
             _ => reset_matching_input = false,
         }
