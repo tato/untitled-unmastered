9b68918c903cfe2ba8f827e5616d303ef5e35701
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-04-17 10:07:46 +0200

    Mix cursor logic with buffer STUB

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,45 +1,43 @@
 extern crate linked_list;
 use linked_list::LinkedList;
+use std::rc::Rc;
 use crate::*;
 
-#[derive(Copy, Clone, Debug)]
-enum PieceSource {
-    ORIGINAL,
-    APPEND,
-}
 #[derive(Debug)]
 struct Piece {
     start: usize,
     length: usize,
-    source: PieceSource,
+    source: Rc<Vec<u8>>,
 }
 pub struct Buffer {
-    original: Vec<u8>,
-    append: Vec<u8>,
-    pieces: LinkedList<Piece>,
-}
+    original: Rc<Vec<u8>>,
+    append: Rc<Vec<u8>>,
+    pieces: LinkedList<Rc<Piece>>,
 
-use PieceSource::*;
+    cursor_piece: Rc<Piece>,
+
+    reminder_cursor_x: usize,
+    cursor_x: usize,
+    cursor_y: usize,
+}
 impl Buffer {
-    pub fn new() -> Self {
-        Self {
-            original: Vec::new(),
-            append: Vec::new(),
-            pieces: LinkedList::new(),
-        }
-    }
     pub fn from(source: &str) -> Self {
-        let first_piece = Piece {
+        let original = source.to_string().into_bytes();
+        let first_piece: Rc<Piece> = Piece {
             start: 0,
             length: source.len(),
-            source: ORIGINAL,
-        };
+            source: original.into(),
+        }.into();
         let mut pieces = LinkedList::new();
-        pieces.push_back(first_piece);
+        pieces.push_back(first_piece.clone());
         Self {
-            original: source.to_string().into_bytes(),
-            append: Vec::new(),
             pieces,
+            original: Vec::new().into(),
+            append: Vec::new().into(),
+            cursor_piece: first_piece.clone(),
+            reminder_cursor_x: 0,
+            cursor_x: 0,
+            cursor_y: 0,
         }
     }
 
@@ -56,11 +54,8 @@ impl Buffer {
 
             let piece_start = piece.start;
             let piece_length = piece.length;
-            let piece_source = piece.source;
-            let from = match &piece.source {
-                ORIGINAL => &self.original,
-                APPEND => &self.append,
-            };
+            let piece_source = piece.source.clone();
+            let from = &piece_source;
 
             cursor.prev();
             cursor.remove();
@@ -80,9 +75,9 @@ impl Buffer {
                 let after = Piece {
                     start: piece_start + remove_offset + len,
                     length: piece_length - remove_offset - len,
-                    source: piece_source,
+                    source: piece_source.clone(),
                 };
-                cursor.insert(after);
+                cursor.insert(after.into());
             }
 
             if position != start_in_render {
@@ -92,18 +87,19 @@ impl Buffer {
                     source: piece_source,
                 };
 
-                cursor.insert(before);
+                cursor.insert(before.into());
             }
 
             break;
         }
+        self.recalculate_cursor_piece();
     }
 
     pub fn insert(&mut self, text: &str, insert_position: usize) {
         let start = self.append.len();
         let mut length = 0;
         for c in text.bytes() {
-            self.append.push(c);
+            Rc::get_mut(&mut self.append).unwrap().push(c);
             length += 1;
         }
 
@@ -111,8 +107,8 @@ impl Buffer {
             self.pieces.push_back(Piece {
                 start,
                 length,
-                source: APPEND,
-            });
+                source: self.append.clone(),
+            }.into());
         }
 
         let mut search_position = 0;
@@ -124,7 +120,7 @@ impl Buffer {
 
             if search_position >= insert_position {
                 let piece_start = piece.start;
-                let piece_source = piece.source;
+                let piece_source = piece.source.clone();
 
                 cursor.prev();
                 cursor.remove();
@@ -133,67 +129,147 @@ impl Buffer {
                     let after = Piece {
                         start: piece_start + insert_position - piece_position_start,
                         length: piece_position_end - insert_position,
-                        source: piece_source,
+                        source: piece_source.clone(),
                     };
                     if after.length > 0 {
-                        cursor.insert(after);
+                        cursor.insert(after.into());
                     }
                 }
 
                 let new = Piece {
                     start,
                     length,
-                    source: APPEND,
+                    source: self.append.clone(),
                 };
                 if new.length > 0 {
-                    cursor.insert(new);
+                    cursor.insert(new.into());
                 }
 
                 if insert_position != piece_position_start {
                     let before = Piece {
                         start: piece_start,
                         length: insert_position - piece_position_start,
-                        source: piece_source,
+                        source: piece_source.clone(),
                     };
                     if before.length > 0 {
-                        cursor.insert(before);
+                        cursor.insert(before.into());
                     }
                 }
                 break;
             }
         }
+        self.recalculate_cursor_piece();
     }
 
     pub fn to_string(&self) -> String {
         let mut result = String::new();
         for piece in &self.pieces {
-            let from = match &piece.source {
-                ORIGINAL => &self.original,
-                APPEND => &self.append,
-            };
+            let from = &piece.source;
             let sl = &from[piece.start..piece.start + piece.length];
             result += std::str::from_utf8(sl).unwrap_or_else(panic_with_dialog);
         }
         result
     }
 
-    pub fn get(&mut self, idx: usize) -> Option<&str> {
-        let mut current = 0;
-        for piece in &self.pieces {
-            let next = current + piece.length;
-            if idx >= current && idx < next {
-                let from = match &piece.source {
-                    ORIGINAL => &self.original,
-                    APPEND => &self.append,
-                };
-                let s = unsafe {
-                    std::str::from_utf8_unchecked(&from[piece.start..piece.length])
-                };
-                return UnicodeSegmentation::graphemes(s, true)
-                    .nth(idx - current + piece.start);
-            }
-            current = next;
-        }
-        None
+    fn recalculate_cursor_piece(&mut self) {
+        unimplemented!();
     }
+
+    pub fn cursor_offset_in_buffer(&self) -> usize {
+        todo!();
+    //     let mut result = 0;
+    //     let mut x = 0;
+    //     let mut y = 0;
+    //     for piece in &self.table.pieces {
+    //         let from = match &piece.source {
+    //             ORIGINAL => &self.original,
+    //             APPEND => &self.append,
+    //         };
+    //         for c in UnicodeSegmentation::graphemes(
+    //         result += piece.length;
+    //     }
+    //     return result;
+    }
+
+    pub fn get_character_under_cursor(&self) -> &str {
+        todo!();
+    }
+
+    // pub fn get(&mut self, idx: usize) -> Option<&str> {
+    //     let mut current = 0;
+    //     for piece in &self.pieces {
+    //         let next = current + piece.length;
+    //         if idx >= current && idx < next {
+    //             let from = match &piece.source {
+    //                 ORIGINAL => &self.original,
+    //                 APPEND => &self.append,
+    //             };
+    //             let s = unsafe {
+    //                 std::str::from_utf8_unchecked(&from[piece.start..piece.length])
+    //             };
+    //             return UnicodeSegmentation::graphemes(s, true)
+    //                 .nth(idx - current + piece.start);
+    //         }
+    //         current = next;
+    //     }
+    //     None
+    // }
+
+    pub fn move_cursor_horizontal(&mut self, x: i64) {
+        todo!();
+        // let length_of_line = self
+        //     .buffer
+        //     .to_string()
+        //     .split('\n')
+        //     .nth(self.cursor_y)
+        //     .map(|it| UnicodeSegmentation::graphemes(it, true).count())
+        //     .unwrap_or(0);
+
+        // let moving_beyond_first = x < 0 && self.global_cursor_x == 0;
+        // let moving_beyond_last = x > 0 && self.global_cursor_x >= length_of_line;
+
+        // if !moving_beyond_first && !moving_beyond_last {
+        //     self.global_cursor_x = ((self.global_cursor_x as i64) + x) as usize;
+        // }
+
+        // self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
+    }
+
+    pub fn move_cursor_vertical(&mut self, y: i64, render: &RenderContext) {
+        todo!();
+
+        // let buffer_string = self.buffer.to_string();
+        // let buffer_lines: Vec<_> = buffer_string
+        //     .split('\n')
+        //     .map(|it| UnicodeSegmentation::graphemes(it, true).collect::<Vec<_>>())
+        //     .collect();
+
+        // let moving_beyond_first = y < 0 && self.cursor_y == 0;
+        // let moving_beyond_last = y > 0 && self.cursor_y >= buffer_lines.len()-1;
+
+        // if !moving_beyond_first && !moving_beyond_last {
+        //     self.cursor_y = ((self.cursor_y as i64) + y) as usize;
+        // }
+
+        // let length_of_line = buffer_lines[self.cursor_y].len();
+        // self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
+
+        // let ch = render.character_height;
+        // let window_height_in_characters = (render.height() / ch) as usize;
+        // if y > 0 &&
+        //     self.cursor_y > self.y_render_offset + window_height_in_characters - 7
+        // {
+        //     self.y_render_offset += y as usize;
+        // }
+        // if y < 0 && self.cursor_y < self.y_render_offset + 5
+        //     && (self.y_render_offset as i64) + y >= 0
+        // {
+        //     self.y_render_offset -= (-y) as usize;
+        // }
+        // self.y_render_offset = min(self.y_render_offset, buffer_lines.len());
+
+        // self.cursor_animation_instant = Instant::now();
+    }
+
 }
+
--- src/editor.rs
+++ src/editor.rs
@@ -16,9 +16,6 @@ pub struct Editor {
 
     pub editing_file_path: String,
 
-    global_cursor_x: usize,
-    actual_cursor_x: usize,
-    cursor_y: usize,
     pub cursor_animation_instant: Instant,
 
     pub matching_input_text: String,
@@ -31,14 +28,11 @@ impl Editor {
         Self {
             mode: Mode::NORMAL,
 
-            buffer: buffer::Buffer::new(),
+            buffer: buffer::Buffer::from(""),
             y_render_offset: 0,
 
             editing_file_path: String::from(""),
 
-            global_cursor_x: 0,
-            actual_cursor_x: 0,
-            cursor_y: 0,
             cursor_animation_instant: Instant::now(),
 
             matching_input_text: String::new(),
@@ -47,79 +41,28 @@ impl Editor {
         }
     }
 
-    pub fn move_cursor_horizontal(&mut self, x: i64) {
-        let length_of_line = self
-            .buffer
-            .to_string()
-            .split('\n')
-            .nth(self.cursor_y)
-            .map(|it| UnicodeSegmentation::graphemes(it, true).count())
-            .unwrap_or(0);
-
-        let moving_beyond_first = x < 0 && self.global_cursor_x == 0;
-        let moving_beyond_last = x > 0 && self.global_cursor_x >= length_of_line;
-
-        if !moving_beyond_first && !moving_beyond_last {
-            self.global_cursor_x = ((self.global_cursor_x as i64) + x) as usize;
-        }
-
-        self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
-    }
-
-    pub fn move_cursor_vertical(&mut self, y: i64, render: &RenderContext) {
-        let buffer_string = self.buffer.to_string();
-        let buffer_lines: Vec<_> = buffer_string
-            .split('\n')
-            .map(|it| UnicodeSegmentation::graphemes(it, true).collect::<Vec<_>>())
-            .collect();
-
-        let moving_beyond_first = y < 0 && self.cursor_y == 0;
-        let moving_beyond_last = y > 0 && self.cursor_y >= buffer_lines.len()-1;
-
-        if !moving_beyond_first && !moving_beyond_last {
-            self.cursor_y = ((self.cursor_y as i64) + y) as usize;
-        }
-
-        let length_of_line = buffer_lines[self.cursor_y].len();
-        self.actual_cursor_x = min(self.global_cursor_x, length_of_line);
-
-        let ch = render.character_height;
-        let window_height_in_characters = (render.height() / ch) as usize;
-        if y > 0 &&
-            self.cursor_y > self.y_render_offset + window_height_in_characters - 7
-        {
-            self.y_render_offset += y as usize;
-        }
-        if y < 0 && self.cursor_y < self.y_render_offset + 5
-            && (self.y_render_offset as i64) + y >= 0
-        {
-            self.y_render_offset -= (-y) as usize;
-        }
-        self.y_render_offset = min(self.y_render_offset, buffer_lines.len());
-
-        self.cursor_animation_instant = Instant::now();
-    }
-
     pub fn cursor(&self) -> (usize,usize) {
-        (self.actual_cursor_x,self.cursor_y)
+        todo!();
+        // (self.actual_cursor_x,self.cursor_y)
     }
 
     pub fn cursor_position_in_buffer(&self) -> usize {
-        let buffer_string = self.buffer.to_string();
-        let buffer_lines: Vec<&str> = buffer_string
-            .split('\n')
-            .take(self.cursor_y + 1)
-            .collect();
-        let length_before_line = buffer_lines[0..self.cursor_y]
-            .iter()
-            .map(|t| t.len() + 1)
-            .sum::<usize>();
-        let length_inside_line = UnicodeSegmentation::
-            graphemes(buffer_lines[self.cursor_y], true)
-            .take(self.actual_cursor_x)
-            .map(|gc| gc.len())
-            .sum::<usize>();
-        length_before_line + length_inside_line
+        todo!();
+        // let buffer_string = self.buffer.to_string();
+        // let buffer_lines: Vec<&str> = buffer_string
+        //     .split('\n')
+        //     .take(self.cursor_y + 1)
+        //     .collect();
+        // let length_before_line = buffer_lines[0..self.cursor_y]
+        //     .iter()
+        //     .map(|t| t.len() + 1)
+        //     .sum::<usize>();
+        // let length_inside_line = UnicodeSegmentation::
+        //     graphemes(buffer_lines[self.cursor_y], true)
+        //     .take(self.actual_cursor_x)
+        //     .map(|gc| gc.len())
+        //     .sum::<usize>();
+        // length_before_line + length_inside_line
     }
 
     pub fn handle_input(&mut self, render: &RenderContext, text: &str, modifs: u32, is_text_input: bool) {
@@ -141,26 +84,27 @@ impl Editor {
         match (mit, mim) {
             binding!(i) => self.mode = Mode::INSERT,
             binding!(a) => {
-                self.move_cursor_horizontal(1);
+                self.buffer.move_cursor_horizontal(1);
                 self.mode = Mode::INSERT;
             },
-            binding!(h) => self.move_cursor_horizontal(-1),
-            binding!(l) => self.move_cursor_horizontal(1),
-            binding!(k) => self.move_cursor_vertical(-1, render),
-            binding!(j) => self.move_cursor_vertical(1, render),
+            binding!(h) => self.buffer.move_cursor_horizontal(-1),
+            binding!(l) => self.buffer.move_cursor_horizontal(1),
+            binding!(k) => self.buffer.move_cursor_vertical(-1, render),
+            binding!(j) => self.buffer.move_cursor_vertical(1, render),
             binding!(e) | binding!(E) => {
-                loop {
-                    match self.buffer.get(self.cursor_position_in_buffer()) {
-                        None => break,
-                        Some(c) => {
-                            if c == " " || c == "\n" {
-                                self.move_cursor_horizontal(-1);
-                                break;
-                            }
-                            self.move_cursor_horizontal(1);
-                        },
-                    }
-                }
+                todo!();
+                // loop {
+                //     match self.get(self.cursor_position_in_buffer()) {
+                //         None => break,
+                //         Some(c) => {
+                //             if c == " " || c == "\n" {
+                //                 self.buffer.move_cursor_horizontal(-1);
+                //                 break;
+                //             }
+                //             self.buffer.move_cursor_horizontal(1);
+                //         },
+                //     }
+                // }
             }
             binding!(d, d) => println!("dd is nice!"),
             binding!(CTRL+a, b, CTRL+c) => println!("abc is nice!"),
@@ -179,23 +123,25 @@ impl Editor {
         let mim: &[u32] = &self.matching_input_modifs;
         match (mit, mim) {
             binding!(BACKSPACE) => {
-                if self.actual_cursor_x != 0 || self.cursor_y != 0 {
-                    self.move_cursor_horizontal(-1);
-                    let pos = self.cursor_position_in_buffer();
-                    self.buffer.remove(pos);
-                }
+                todo!();
+                // if self.actual_cursor_x != 0 || self.cursor_y != 0 {
+                //     self.move_cursor_horizontal(-1);
+                //     let pos = self.cursor_position_in_buffer();
+                //     self.buffer.remove(pos);
+                // }
             }
             binding!(RETURN) => {
                 let pos = self.cursor_position_in_buffer();
                 self.buffer.insert("\n", pos);
-                self.move_cursor_vertical(1, render);
-                self.global_cursor_x = 0;
-                self.actual_cursor_x = 0;
+                todo!();
+                // self.move_cursor_vertical(1, render);
+                // self.global_cursor_x = 0;
+                // self.actual_cursor_x = 0;
             }
-            binding!(LEFT) => self.move_cursor_horizontal(-1),
-            binding!(RIGHT) => self.move_cursor_horizontal(1),
-            binding!(UP) => self.move_cursor_vertical(-1, render),
-            binding!(DOWN) => self.move_cursor_vertical(1, render),
+            binding!(LEFT) => self.buffer.move_cursor_horizontal(-1),
+            binding!(RIGHT) => self.buffer.move_cursor_horizontal(1),
+            binding!(UP) => self.buffer.move_cursor_vertical(-1, render),
+            binding!(DOWN) => self.buffer.move_cursor_vertical(1, render),
             binding!(CTRL+o) => {
                 let result = nfd::open_file_dialog(None, None)
                     .unwrap_or_else(panic_with_dialog);
@@ -219,7 +165,7 @@ impl Editor {
             _ if is_text_input => {
                 let pos = self.cursor_position_in_buffer();
                 self.buffer.insert(input, pos);
-                self.move_cursor_horizontal(1);
+                self.buffer.move_cursor_horizontal(1);
             },
             _ => reset_matching_input = false,
         }
