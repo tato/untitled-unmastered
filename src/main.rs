f0f2a57eb995c7bd48238ccd1737ea0b2045d7df
Pablo Tato Ramos <pabtatoramos@gmail.com>
2020-04-18 15:19:41 +0200

    Rename internal crate

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use unicode_segmentation::UnicodeSegmentation;
-use rust_macros::*;
+use uu_rust_macros::*;
 use std::time::{Duration,Instant};
 use std::cmp::min;
 
