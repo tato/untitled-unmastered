71583bc169c44cbd1caaf65ab7c00a93c71c3eb1
Pablo Tato Ramos <pabtatoramos@gmail.com>
2021-01-12 23:32:17 +0100

    apparently this implementation doesnt do hidpi

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 #![allow(clippy::new_without_default)]
 
-// use sdl2::keyboard;
-// use sdl2::keyboard::Keycode;
-// use sdl2::event::Event;
-// use sdl2::pixels::Color;
-// use sdl2::rect::Rect;
+use glium::glutin;
+use glium::glutin::event::{Event, WindowEvent};
+use glium::glutin::event_loop::{ControlFlow, EventLoop};
+use glium::glutin::window::WindowBuilder;
+use glium::{Display, Surface};
+use imgui::{Context, FontConfig, FontGlyphRanges, FontSource, Ui, im_str, Window, Condition};
+use imgui_glium_renderer::Renderer;
+use imgui_winit_support::{HiDpiMode, WinitPlatform};
+use std::path::Path;
 use unicode_segmentation::UnicodeSegmentation;
 use uu_rust_macros::*;
 use std::time::{Duration,Instant};
@@ -38,6 +42,105 @@ pub mod editor;
 use render::RenderContext;
 
 fn main() {
+    let title = "uu";
+    let title = match Path::new(&title).file_name() {
+        Some(file_name) => file_name.to_str().unwrap(),
+        None => title,
+    };
+
+    let event_loop = EventLoop::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let builder = WindowBuilder::new()
+        .with_title(title.to_owned())
+        .with_inner_size(glutin::dpi::LogicalSize::new(1024f64, 768f64));
+    let display =
+        Display::new(builder, context, &event_loop).expect("Failed to initialize display");
+
+    let mut imgui = Context::create();
+    imgui.set_ini_filename(None);
+
+    // todo!
+    // if let Some(backend) = clipboard::init() {
+    //     imgui.set_clipboard_backend(Box::new(backend));
+    // } else {
+    //     eprintln!("Failed to initialize clipboard");
+    // }
+
+    let mut platform = WinitPlatform::init(&mut imgui);
+    {
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
+        platform.attach_window(imgui.io_mut(), window, HiDpiMode::Rounded);
+    }
+
+    let hidpi_factor = platform.hidpi_factor();
+    let font_size = (13.0 * hidpi_factor) as f32;
+    imgui.fonts().add_font(&[
+        FontSource::DefaultFontData {
+            config: Some(FontConfig {
+                size_pixels: font_size,
+                ..FontConfig::default()
+            }),
+        },
+        FontSource::TtfData {
+            data: include_bytes!("Cousine-Regular.ttf"),
+            size_pixels: font_size,
+            config: Some(FontConfig {
+                rasterizer_multiply: 1.75,
+                glyph_ranges: FontGlyphRanges::japanese(),
+                ..FontConfig::default()
+            }),
+        },
+    ]);
+
+    imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
+
+    let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
+
+    let mut last_frame = Instant::now();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::NewEvents(_) => {
+            let now = Instant::now();
+            imgui.io_mut().update_delta_time(now - last_frame);
+            last_frame = now;
+        }
+        Event::MainEventsCleared => {
+            let gl_window = display.gl_window();
+            platform
+                .prepare_frame(imgui.io_mut(), gl_window.window())
+                .expect("Failed to prepare frame");
+            gl_window.window().request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            let mut ui = imgui.frame();
+
+            let mut run = true;
+            run_ui(&mut run, &mut ui);
+            if !run {
+                *control_flow = ControlFlow::Exit;
+            }
+
+            let gl_window = display.gl_window();
+            let mut target = display.draw();
+            target.clear_color_srgb(1.0, 1.0, 1.0, 1.0);
+            platform.prepare_render(&ui, gl_window.window());
+            let draw_data = ui.render();
+            renderer
+                .render(&mut target, draw_data)
+                .expect("Rendering failed");
+            target.finish().expect("Failed to swap buffers");
+        }
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => *control_flow = ControlFlow::Exit,
+        event => {
+            let gl_window = display.gl_window();
+            platform.handle_event(imgui.io_mut(), gl_window.window(), &event);
+        }
+    })
+
     // let sdl_context = sdl2::init().unwrap_or_else(panic_with_dialog);
     // let video_context = sdl_context.video().unwrap_or_else(panic_with_dialog);
     // let ttf_context = sdl2::ttf::init().unwrap_or_else(panic_with_dialog);
@@ -225,4 +328,20 @@ fn main() {
     //     editor.fade_matching_input(frame_duration);
     //     previous_frame_instant = Instant::now();
     // }
+}
+
+fn run_ui(run: &mut bool, ui: &mut Ui) {
+    Window::new(im_str!("Hello world"))
+        .size([300.0, 110.0], Condition::FirstUseEver)
+        .build(ui, || {
+            ui.text(im_str!("Hello world!"));
+            ui.text(im_str!("こんにちは世界！"));
+            ui.text(im_str!("This...is...imgui-rs!"));
+            ui.separator();
+            let mouse_pos = ui.io().mouse_pos;
+            ui.text(format!(
+                "Mouse Position: ({:.1},{:.1})",
+                mouse_pos[0], mouse_pos[1]
+            ));
+        });
 }
