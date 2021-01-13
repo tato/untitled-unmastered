f5665686e224cd79eff56b1737e9a65e4a8bcab2
Pablo Tato Ramos <pabtatoramos@gmail.com>
2021-01-13 19:36:10 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 #![allow(clippy::new_without_default)]
 
+use editor::Editor;
 use glium::glutin;
 use glium::glutin::event::{Event, WindowEvent};
 use glium::glutin::event_loop::{ControlFlow, EventLoop};
 use glium::glutin::window::WindowBuilder;
 use glium::{Display, Surface};
-use imgui::{Context, FontConfig, FontGlyphRanges, FontSource, Ui, im_str, Window, Condition};
+use imgui::{ClipboardBackend, Condition, Context, FontConfig, FontGlyphRanges, FontSource, ImStr, ImString, Ui, Window, im_str};
 use imgui_glium_renderer::Renderer;
 use imgui_winit_support::{HiDpiMode, WinitPlatform};
-use std::path::Path;
+use clipboard::{self, ClipboardContext, ClipboardProvider};
 use unicode_segmentation::UnicodeSegmentation;
 use uu_rust_macros::*;
 use std::time::{Duration,Instant};
 use std::cmp::min;
 
 pub fn panic_with_dialog<Any>(m: impl std::fmt::Display) -> Any {
-    todo!("panic_with_dialog");
     // let owned = m.to_string();
     // sdl2::messagebox::show_simple_message_box(
     //     sdl2::messagebox::MessageBoxFlag::ERROR,
@@ -37,34 +37,36 @@ pub fn panic_with_dialog<Any>(m: impl std::fmt::Display) -> Any {
 // }
 
 pub mod buffer;
-pub mod render;
 pub mod editor;
-use render::RenderContext;
+pub mod render;
+
+pub struct ClipboardSupport(ClipboardContext);
+impl ClipboardBackend for ClipboardSupport {
+    fn get(&mut self) -> Option<ImString> {
+        self.0.get_contents().ok().map(|text| text.into())
+    }
+    fn set(&mut self, text: &ImStr) {
+        let _ = self.0.set_contents(text.to_str().to_owned());
+    }
+}
 
 fn main() {
-    let title = "uu";
-    let title = match Path::new(&title).file_name() {
-        Some(file_name) => file_name.to_str().unwrap(),
-        None => title,
-    };
-
     let event_loop = EventLoop::new();
     let context = glutin::ContextBuilder::new().with_vsync(true);
     let builder = WindowBuilder::new()
-        .with_title(title.to_owned())
-        .with_inner_size(glutin::dpi::LogicalSize::new(1024f64, 768f64));
+        .with_title("uu".to_owned())
+        .with_maximized(true);
     let display =
         Display::new(builder, context, &event_loop).expect("Failed to initialize display");
 
     let mut imgui = Context::create();
     imgui.set_ini_filename(None);
 
-    // todo!
-    // if let Some(backend) = clipboard::init() {
-    //     imgui.set_clipboard_backend(Box::new(backend));
-    // } else {
-    //     eprintln!("Failed to initialize clipboard");
-    // }
+    if let Ok(context) = ClipboardContext::new() {
+        imgui.set_clipboard_backend(Box::new(ClipboardSupport(context)));
+    } else {
+        eprintln!("Failed to initialize clipboard");
+    }
 
     let mut platform = WinitPlatform::init(&mut imgui);
     {
@@ -101,6 +103,15 @@ fn main() {
     imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
 
     let mut renderer = Renderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
+    
+    // let character_width = render.character_width;
+    // let character_height = render.character_height;
+
+    // let foreground_color = Color::RGB(0, 0, 0);
+    // let background_color = Color::RGB(250, 250, 250);
+
+    let mut editor = editor::Editor::new();
+    editor.buffer = buffer::Buffer::from(include_str!("main.rs"));
 
     let mut last_frame = Instant::now();
 
@@ -121,7 +132,7 @@ fn main() {
             let mut ui = imgui.frame();
 
             let mut run = true;
-            run_ui(&mut run, &mut ui);
+            run_ui(&mut run, &mut ui, &mut editor);
             if !run {
                 *control_flow = ControlFlow::Exit;
             }
@@ -145,105 +156,23 @@ fn main() {
             platform.handle_event(imgui.io_mut(), gl_window.window(), &event);
         }
     })
+}
 
-    // let sdl_context = sdl2::init().unwrap_or_else(panic_with_dialog);
-    // let video_context = sdl_context.video().unwrap_or_else(panic_with_dialog);
-    // let ttf_context = sdl2::ttf::init().unwrap_or_else(panic_with_dialog);
-    // let window = video_context
-    //     .window("uu", 10, 10)
-    //     .maximized()
-    //     .position_centered()
-    //     .opengl()
-    //     .build()
-    //     .unwrap_or_else(panic_with_dialog);
-    // let mut canvas = window
-    //     .into_canvas()
-    //     .present_vsync()
-    //     .build()
-    //     .unwrap_or_else(panic_with_dialog);
-    // let texture_creator = canvas.texture_creator();
-    // let mut render = RenderContext::new(&mut canvas, &texture_creator, &ttf_context);
-
-    // let character_width = render.character_width;
-    // let character_height = render.character_height;
-
-    // let foreground_color = Color::RGB(0, 0, 0);
-    // let background_color = Color::RGB(250, 250, 250);
-
-    // let mut editor = editor::Editor::new();
-    // editor.buffer = buffer::Buffer::from(include_str!("main.rs"));
-
-    // let mut event_pump = sdl_context
-    //     .event_pump()
-    //     .unwrap_or_else(panic_with_dialog);
-
-    // let mut previous_frame_instant = Instant::now();
-
-    // 'running: loop {
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit { .. } => {
-    //                 break 'running;
-    //             }
-    //             Event::KeyDown { keycode: Some(keycode), keymod, .. } => {
-    //                 let mut modifs = 0_u32;
-    //                 if keymod.contains(keyboard::Mod::LCTRLMOD) {
-    //                     modifs |= CTRL!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RCTRLMOD) {
-    //                     modifs |= CTRL!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::LSHIFTMOD) {
-    //                     modifs |= SHIFT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RSHIFTMOD) {
-    //                     modifs |= SHIFT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::LALTMOD) {
-    //                     modifs |= ALT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RALTMOD) {
-    //                     modifs |= ALT!();
-    //                 }
-
-    //                 let is_text_input = false;
-    //                 if let Some(gc) = get_utf8_for_keycode(keycode) {
-    //                     editor.handle_input(&render, gc, modifs, is_text_input);
-    //                 }
-    //             }
-    //             Event::TextInput { text, .. } => {
-    //                 let keymod = sdl_context.keyboard().mod_state();
-    //                 let mut modifs = 0_u32;
-
-    //                 if keymod.contains(keyboard::Mod::LCTRLMOD) {
-    //                     modifs |= CTRL!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RCTRLMOD) {
-    //                     modifs |= CTRL!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::LSHIFTMOD) {
-    //                     modifs |= SHIFT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RSHIFTMOD) {
-    //                     modifs |= SHIFT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::LALTMOD) {
-    //                     modifs |= ALT!();
-    //                 }
-    //                 if keymod.contains(keyboard::Mod::RALTMOD) {
-    //                     modifs |= ALT!();
-    //                 }
-
-    //                 let is_text_input = true;
-    //                 editor.handle_input(&render, &text, modifs, is_text_input);
-    //             }
-
-    //             _ => {}
-    //         }
-    //     }
-
-    //     render.start_frame(background_color);
-
+fn run_ui(_run: &mut bool, ui: &mut Ui, _editor: &mut Editor) {
+    Window::new(im_str!("Hello world"))
+        .size([300.0, 110.0], Condition::FirstUseEver)
+        .build(ui, || {
+            ui.text(im_str!("Hello world!"));
+            ui.text(im_str!("こんにちは世界！"));
+            ui.text(im_str!("This...is...imgui-rs!"));
+            ui.separator();
+            let mouse_pos = ui.io().mouse_pos;
+            ui.text(format!(
+                "Mouse Position: ({:.1},{:.1})",
+                mouse_pos[0], mouse_pos[1]
+            ));
+        });
+    
     //     let cursor_color_ms_interval = 500;
     //     let elapsed_ms = editor.cursor_animation_instant.elapsed().as_millis();
     //     let cursor_color = if (elapsed_ms / cursor_color_ms_interval) % 2 == 0 {
@@ -333,20 +262,4 @@ fn main() {
     //     editor.fade_matching_input(frame_duration);
     //     previous_frame_instant = Instant::now();
     // }
-}
-
-fn run_ui(run: &mut bool, ui: &mut Ui) {
-    Window::new(im_str!("Hello world"))
-        .size([300.0, 110.0], Condition::FirstUseEver)
-        .build(ui, || {
-            ui.text(im_str!("Hello world!"));
-            ui.text(im_str!("こんにちは世界！"));
-            ui.text(im_str!("This...is...imgui-rs!"));
-            ui.separator();
-            let mouse_pos = ui.io().mouse_pos;
-            ui.text(format!(
-                "Mouse Position: ({:.1},{:.1})",
-                mouse_pos[0], mouse_pos[1]
-            ));
-        });
 }
