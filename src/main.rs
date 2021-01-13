f3c60062e2cdd37f9d56cd05a255adacc5a1a69f
Pablo Tato Ramos <pabtatoramos@gmail.com>
2021-01-13 18:57:33 +0100

    so that was dpi huh...

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ fn main() {
     {
         let gl_window = display.gl_window();
         let window = gl_window.window();
-        platform.attach_window(imgui.io_mut(), window, HiDpiMode::Rounded);
+        platform.attach_window(imgui.io_mut(), window, HiDpiMode::Default);
     }
 
     let hidpi_factor = platform.hidpi_factor();
